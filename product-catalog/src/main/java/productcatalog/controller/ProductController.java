80c47433adaf4ef63133a8d3cb8029af3a971eb3
Robin Lamatao <RobinLamatao@gmail.com>
2023-02-25 01:46:33 +0800

    Create Product Form UI and POST to server for creation of product.

--- product-catalog/src/main/java/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/productcatalog/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import productcatalog.data.Product;
 import productcatalog.dto.ProductDTO;
+import productcatalog.service.interfaces.CategoryService;
 import productcatalog.service.interfaces.ProductService;
 
 @Controller
@@ -16,11 +18,23 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private CategoryService categoryService;
+	
+	
 	@GetMapping("/products")
 	public String getProducts(Model model) {
 		 List<ProductDTO> products = productService.getAllProducts();
 	        model.addAttribute("products", products);
 	        return "products";
+	}
+	
+	@GetMapping("/products/create")
+	public String showCreateProductForm(Model model) {
+		
+		model.addAttribute("product", new Product());
+		model.addAttribute("categories", categoryService.getAllCategories());
+		return "create-product";
 	}
 
 }
